50e8642e3f8ccb059de13b8682c4d771e0486226
mukhonishfa <mukhonishfa28@gmail.com>
2025-05-06 19:27:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ html_temp = """<div style="background-color:#000;padding:10px;border-radius:10px
 desc_temp = """ ### Song Popularity Prediction App 
                 This app is used by Cakrawala Team for predicting Song Popularity
                 
-                ### Data Source
+desc_temp = """ ### Data Source
                 Kaggle: https://www.kaggle.com/datasets/yasserh/song-popularity-dataset
                 """
 
